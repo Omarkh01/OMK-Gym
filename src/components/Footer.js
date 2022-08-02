bf18eaece40b078be6a43aa84c70e60f51624d47
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-08-01 23:07:44 -0500

    pushing Ox-Gym fitness app code

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from '../assets/images/Logo-1.png'
+
+const Footer = () => {
+  return (
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' width='200px' height='40px' />
+        <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
+          Made With ❤️ by Omar Khuatbyek
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Footer
